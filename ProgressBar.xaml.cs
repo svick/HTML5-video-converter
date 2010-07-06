acbe0874d46d049c41c4deba16c5daad01640f86
Petr Onderka <gsvick@gmail.com>
2010-07-06 23:38:21 +0200

    ProgressBar cancel

--- ProgressBar.xaml.cs
+++ ProgressBar.xaml.cs
@@ -1,26 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Video_converter
 {
-	/// <summary>
-	/// Interaction logic for ProgressBar.xaml
-	/// </summary>
 	public partial class ProgressBar : UserControl
 	{
+		public event EventHandler Cancelled = delegate { };
+
 		public ProgressBar()
 		{
 			InitializeComponent();
+		}
+
+		private void Cancel_Click(object sender, RoutedEventArgs e)
+		{
+			Cancelled(this, EventArgs.Empty);
 		}
 	}
 }
