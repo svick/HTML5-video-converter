759bb235949879dbefcde64f143cad65bbfac98b
Petr Onderka <gsvick@gmail.com>
2010-07-19 12:08:31 +0200

    better TimeSpanExtension

--- TimeSpanExtension.cs
+++ TimeSpanExtension.cs
@@ -1,57 +1,71 @@
 ﻿using System;
+using System.Linq;
 
 namespace Video_converter
 {
 	static class TimeSpanExtension
 	{
-		static readonly string[] secondForms = new string[] { "sekunda", "sekundy", "sekund" };
-		static readonly string[] minuteForms = new string[] { "minuta", "minuty", "minut" };
-		static readonly string[] hourForms = new string[] { "hodina", "hodiny", "hodin" };
-		static readonly string[] dayForms = new string[] { "den", "dny", "dní" };
+		class Unit
+		{
+			static readonly string oneUnitFormat = "{0} {1}";
 
-		static readonly string oneUnitFormat = "{0} {1}";
+			Func<TimeSpan, int> selector;
+			string[] forms;
+
+			public Unit(Func<TimeSpan, int> selector, string[] forms)
+			{
+				this.selector = selector;
+				this.forms = forms;
+			}
+
+			public bool NotNull(TimeSpan timeSpan)
+			{
+				return selector(timeSpan) != 0;
+			}
+
+			public string Format(TimeSpan timeSpan)
+			{
+				return string.Format(oneUnitFormat, selector(timeSpan), pickForm(selector(timeSpan)));
+			}
+
+			string pickForm(int number)
+			{
+				number = Math.Abs(number);
+				if (number == 1)
+					return forms[0];
+				else if (number <= 4)
+					return forms[1];
+				else
+					return forms[2];
+			}
+		}
+
+		static readonly Unit[] units = new Unit[] {
+			new Unit(ts => ts.Days, new string[] { "den", "dny", "dní" }),
+			new Unit(ts => ts.Hours, new string[] { "hodina", "hodiny", "hodin" }),
+			new Unit(ts => ts.Minutes, new string[] { "minuta", "minuty", "minut" }),
+			new Unit(ts => ts.Seconds, new string[] { "sekunda", "sekundy", "sekund" })
+		};
 
 		public static string ToLongString(this TimeSpan timeSpan)
 		{
-			if (timeSpan.Days != 0)
-				return DayString(timeSpan) + " a " + HourString(timeSpan);
-			else if (timeSpan.Hours != 0)
-				return HourString(timeSpan) + " a " + MinuteString(timeSpan);
-			else if (timeSpan.Minutes != 0)
-				return MinuteString(timeSpan) + " a " + SecondString(timeSpan);
-			else
-				return SecondString(timeSpan);
-		}
+			string result = null;
+			foreach (Unit unit in units)
+			{
+				if (result != null)
+				{
+					result += " a " + unit.Format(timeSpan);
+					break;
+				}
+				else
+					if (unit.NotNull(timeSpan))
+						result = unit.Format(timeSpan);
+			}
 
-		public static string DayString(TimeSpan timeSpan)
-		{
-			return string.Format(oneUnitFormat, timeSpan.Days, pickForm(timeSpan.Days, dayForms));
-		}
+			if (result == null)
+				result = units.Last().Format(timeSpan);
 
-		public static string HourString(TimeSpan timeSpan)
-		{
-			return string.Format(oneUnitFormat, timeSpan.Hours, pickForm(timeSpan.Hours, hourForms));
-		}
-
-		public static string MinuteString(TimeSpan timeSpan)
-		{
-			return string.Format(oneUnitFormat, timeSpan.Minutes, pickForm(timeSpan.Minutes, minuteForms));
-		}
-
-		public static string SecondString(TimeSpan timeSpan)
-		{
-			return string.Format(oneUnitFormat, timeSpan.Seconds, pickForm(timeSpan.Seconds, secondForms));
-		}
-
-		static string pickForm(int number, string[] forms)
-		{
-			number = Math.Abs(number);
-			if (number == 1)
-				return forms[0];
-			else if (number <= 4)
-				return forms[1];
-			else
-				return forms[2];
+			return result;
 		}
 	}
 }
