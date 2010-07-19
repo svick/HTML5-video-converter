8991d3871789e0b1a625fdb262e7ff1e09ff36df
Petr Onderka <gsvick@gmail.com>
2010-07-19 22:38:24 +0200

    set WPF localization language

--- App.xaml.cs
+++ App.xaml.cs
@@ -17,6 +17,7 @@ namespace Video_converter
 
 		public App()
 		{
+			WPFLocalizeExtension.Engine.LocalizeDictionary.Instance.Culture = System.Threading.Thread.CurrentThread.CurrentUICulture;
 			locateFFmpegFile();
 		}
 
