c7be548a39c0193ae2ccbd54e894991d0653793f
Petr Onderka <gsvick@gmail.com>
2010-07-22 16:20:22 +0200

    better layout and localization for About

--- About.xaml.cs
+++ About.xaml.cs
@@ -1,26 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Reflection;
 
 namespace Video_converter
 {
-	/// <summary>
-	/// Interaction logic for About.xaml
-	/// </summary>
 	public partial class About : Window
 	{
 		public About()
 		{
 			InitializeComponent();
+			Version version = Assembly.GetExecutingAssembly().GetName().Version;
+			string versionString = string.Format("{0}.{1}", version.Major, version.Minor);
+			versionTextBlock.Text = App.GetLocalizedString("Version", versionString);
 		}
 	}
 }
