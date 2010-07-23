ccea655471517c145bad5976fe2759142045986f
Jakub Onderka <jakub.onderka@gmail.com>
2010-07-23 18:36:22 +0200

    Scroll down at begin

--- LogWindow.xaml.cs
+++ LogWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace Video_converter
 
 		private void TextLog_TextChanged(object sender, TextChangedEventArgs e)
 		{
-			if (TextLog.VerticalOffset + TextLog.ViewportHeight == TextLog.ExtentHeight)
+			if (TextLog.ExtentHeight < TextLog.ViewportHeight || TextLog.VerticalOffset + TextLog.ViewportHeight == TextLog.ExtentHeight)
 				TextLog.ScrollToEnd();
 			else
 				TextLog.ScrollToVerticalOffset(TextLog.VerticalOffset);
