ac14067c04fd92fda062d293acca4a493f5d1946
Jakub Onderka <jakub.onderka@gmail.com>
2010-07-28 10:39:00 +0200

    Tooltip format

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -82,7 +82,7 @@ namespace Video_converter
 						BitRate newBitRate = video.ComputeNewBitRate(newSize);
 						int expectedSize = (int)(((newBitRate.Video + newBitRate.Audio) * video.Duration.TotalSeconds) / 8000); // in MB
 
-						resolutionCheckBox.ToolTip = string.Format("{0} ({1} kb/s, ≈{2} MB)", newSize.ToString("×"), newBitRate.Video, expectedSize);
+						resolutionCheckBox.ToolTip = string.Format("{0}, {1} kb/s, ≈{2} MB", newSize.ToString("×"), newBitRate.Video, expectedSize);
 					}
 				}
 			}
